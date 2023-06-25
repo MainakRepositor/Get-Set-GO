ae06c74bba2a602c69715f1e853a4ebc2b05eaba
MAINAK CHAUDHURI <sultankhilji001@gmail.com>
2023-06-25 11:27:10 +0530

    Create 56.go

---
+++ 56.go
@@ -0,0 +1,21 @@
+// Epoch
+
+package main
+
+import (
+    "fmt"
+    "time"
+)
+
+func main() {
+
+    now := time.Now()
+    fmt.Println(now)
+
+    fmt.Println(now.Unix())
+    fmt.Println(now.UnixMilli())
+    fmt.Println(now.UnixNano())
+
+    fmt.Println(time.Unix(now.Unix(), 0))
+    fmt.Println(time.Unix(0, now.UnixNano()))
+}
