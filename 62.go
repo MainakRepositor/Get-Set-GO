d6d79b803a88abd4ff980ffcfa3cb22bcbc3d582
MAINAK CHAUDHURI <sultankhilji001@gmail.com>
2023-06-25 11:30:42 +0530

    Create 62.go

---
+++ 62.go
@@ -0,0 +1,25 @@
+// Base64 Encoding
+
+package main
+
+import (
+    b64 "encoding/base64"
+    "fmt"
+)
+
+func main() {
+
+    data := "abc123!?$*&()'-=@~"
+
+    sEnc := b64.StdEncoding.EncodeToString([]byte(data))
+    fmt.Println(sEnc)
+
+    sDec, _ := b64.StdEncoding.DecodeString(sEnc)
+    fmt.Println(string(sDec))
+    fmt.Println()
+
+    uEnc := b64.URLEncoding.EncodeToString([]byte(data))
+    fmt.Println(uEnc)
+    uDec, _ := b64.URLEncoding.DecodeString(uEnc)
+    fmt.Println(string(uDec))
+}
