50fa880d94aa3bd11b9f8197236f16239498f85c
MAINAK CHAUDHURI <sultankhilji001@gmail.com>
2023-06-25 11:09:11 +0530

    Create 28.go

---
+++ 28.go
@@ -0,0 +1,16 @@
+// channel buffering
+
+package main
+
+import "fmt"
+
+func main() {
+
+    messages := make(chan string, 2)
+
+    messages <- "buffered"
+    messages <- "channel"
+
+    fmt.Println(<-messages)
+    fmt.Println(<-messages)
+}
