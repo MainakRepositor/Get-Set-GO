b50549c3da85298d6e05a5195d5241d34e573542
MAINAK CHAUDHURI <sultankhilji001@gmail.com>
2023-06-25 11:34:21 +0530

    Create 69.go

---
+++ 69.go
@@ -0,0 +1,29 @@
+// Embed Directives
+
+package main
+
+import (
+    "embed"
+)
+
+//go:embed folder/single_file.txt
+var fileString string
+
+//go:embed folder/single_file.txt
+var fileByte []byte
+
+//go:embed folder/single_file.txt
+//go:embed folder/*.hash
+var folder embed.FS
+
+func main() {
+
+    print(fileString)
+    print(string(fileByte))
+
+    content1, _ := folder.ReadFile("folder/file1.hash")
+    print(string(content1))
+
+    content2, _ := folder.ReadFile("folder/file2.hash")
+    print(string(content2))
+}
