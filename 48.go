aeda611371599afd8d04b674195518d22834ec45
MAINAK CHAUDHURI <sultankhilji001@gmail.com>
2023-06-25 11:22:38 +0530

    Create 48.go

---
+++ 48.go
@@ -0,0 +1,23 @@
+// Recover
+
+package main
+
+import "fmt"
+
+func mayPanic() {
+    panic("a problem")
+}
+
+func main() {
+
+    defer func() {
+        if r := recover(); r != nil {
+
+            fmt.Println("Recovered. Error:\n", r)
+        }
+    }()
+
+    mayPanic()
+
+    fmt.Println("After mayPanic()")
+}
