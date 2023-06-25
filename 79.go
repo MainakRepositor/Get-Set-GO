61f07dde45eb441518cb3ebbdd71e065b0f19a58
MAINAK CHAUDHURI <sultankhilji001@gmail.com>
2023-06-25 11:43:02 +0530

    Create 79.go

---
+++ 79.go
@@ -0,0 +1,26 @@
+// Exec'ing Processes
+
+package main
+
+import (
+    "os"
+    "os/exec"
+    "syscall"
+)
+
+func main() {
+
+    binary, lookErr := exec.LookPath("ls")
+    if lookErr != nil {
+        panic(lookErr)
+    }
+
+    args := []string{"ls", "-a", "-l", "-h"}
+
+    env := os.Environ()
+
+    execErr := syscall.Exec(binary, args, env)
+    if execErr != nil {
+        panic(execErr)
+    }
+}
